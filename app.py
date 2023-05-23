3613fe6eb6f0805e14c9f0773eba291bccd1bd5f
camenduru <54370274+camenduru@users.noreply.github.com>
2023-05-23 04:16:38 +0300

    test

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 import os
-
 os.system(f"git lfs install")
-
 os.system(f"git clone -b v1.2 https://github.com/camenduru/text-generation-webui")
 os.chdir(f"/home/demo/source/text-generation-webui")
-
 os.system(f"pip install -r requirements.txt")
 
-os.system(f"mkdir /home/demo/source/text-generation-webui/repositories")
-os.chdir(f"/home/demo/source/text-generation-webui/repositories")
-os.system(f"git clone -b v1.2 https://github.com/camenduru/GPTQ-for-LLaMa")
-os.chdir(f"GPTQ-for-LLaMa")
-os.system(f"python setup_cuda.py install")
+# os.system(f"mkdir /home/demo/source/text-generation-webui/repositories")
+# os.chdir(f"/home/demo/source/text-generation-webui/repositories")
+# os.system(f"git clone -b v1.2 https://github.com/camenduru/GPTQ-for-LLaMa")
+# os.chdir(f"GPTQ-for-LLaMa")
+# os.system(f"python setup_cuda.py install")
 
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/lmsys/vicuna-13b-delta-v1.1/raw/main/config.json -d /home/demo/source/text-generation-webui/models/vicuna-13b-delta-v1.1 -o config.json")
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/lmsys/vicuna-13b-delta-v1.1/raw/main/generation_config.json -d /home/demo/source/text-generation-webui/models/vicuna-13b-delta-v1.1 -o generation_config.json")
@@ -23,4 +20,5 @@ os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggin
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/lmsys/vicuna-13b-delta-v1.1/resolve/main/pytorch_model-00003-of-00003.bin -d /home/demo/source/text-generation-webui/models/vicuna-13b-delta-v1.1 -o pytorch_model-00003-of-00003.bin")
 
 os.chdir(f"/home/demo/source/text-generation-webui")
+# os.system(f"python server.py --chat --wbits 4 --groupsize 128")
 os.system(f"python server.py --chat")
